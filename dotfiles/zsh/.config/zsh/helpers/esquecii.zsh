56102721d30dbaad30a29f48ff302c51a0b2134f
gui27souza <guilhermeoliveiradesouza@gmail.com>
2026-09-13 01:31:22 -0300

    fix(esquecii): caso de file no root de zsh

--- dotfiles/zsh/.config/zsh/helpers/esquecii.zsh
+++ dotfiles/zsh/.config/zsh/helpers/esquecii.zsh
@@ -7,10 +7,10 @@ esquecii() {
     _formulate_entry() {
         local entry=$1
         local sub_dir=$2
-        
+
         local description=$(grep "^# ZSH_DESC - " "$entry")
         description=${description#\# ZSH_DESC - }
-        
+
         if [[ "$sub_dir" == "true" ]]; then
             echo "  ${${entry%.*}##*/} - $description"
         else
@@ -27,13 +27,13 @@ esquecii() {
             for sub_entry in $(find "$sub_dir" -name "*.zsh"); do
                 _formulate_entry "$sub_entry" "true"
             done
-        
+
         # Se file
-        elif [ -f "$zsh_funcs/$entry" ]; then
+        elif [[ -f "$zsh_funcs/$entry" && "$entry" == *.zsh ]]; then
             echo
             _formulate_entry "$zsh_funcs/$entry" "false"
         fi
-        
+
     done
 
     echo
